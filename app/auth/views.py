9547c9076516c92a9beee2fe7e6e0014e1971522
wsl3 <2350622075@qq.com>
2019-02-23 17:22:13 +0800

    update

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,20 +5,33 @@
 from app.auth import auth
 from app.extensions import db
 from flask import current_app, render_template, request, redirect, url_for, session
-from sqlalchemy import text
+from sqlalchemy import text, or_
 from app.models import Movie, User, Category, Comment
+
 
 @auth.route("/")
 def index():
-    movies = Movie.query.filter(text("id<:id")).params(id=17).all()
+    movies = Movie.query.filter(text("id<:id")).params(id=9).all()
+    country = [""]
+    time = []
     categories = Category.query.all()
     return render_template("auth/index.html", movies=movies, categories=categories)
 
 
-@auth.route("/search/")
-def search():
-    sear = request.args.get('search')
-    return "search:{}".format(sear)
+@auth.route("/search/<int:id>/")
+@auth.route("/search")
+def search(id=0):
+    args = request.args.get('search')
+    if id:
+        t = Category.query.get(id)
+        args = t.type
+        find = t.movies
+        return render_template("auth/search.html", find=find, args=args)
+    elif args:
+        find = Movie.query.filter(Movie.title.like("%{}%".format(args))).all()
+        return render_template("auth/search.html", find=find, args=args)
+    else:
+        redirect(url_for('auth.index'))
 
 
 @auth.route("/login/", methods=["GET", "POST"])
@@ -37,10 +50,12 @@ def login():
         return redirect(url_for('auth.index'))
     return render_template("auth/login.html")
 
+
 @auth.route("/logout/")
 def logout():
     del session["username"]
     return redirect(url_for('auth.index'))
+
 
 @auth.route("/register/", methods=["GET", "POST"])
 def register():
@@ -52,7 +67,7 @@ def register():
 
         if User.query.filter_by(username=username).first():
             return redirect(url_for('auth.register'))
-        if username and password and password==repassword:
+        if username and password and password == repassword:
             user = User(
                 username=username,
                 password=password,
